44bb4f89e825b7d0c71d20d2b1255a7af01d9151
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2019-10-05 00:42:00 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Card from './components/Card';
+
+export default class App extends React.Component {
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Card/>      
+      </View>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactNative from 'react-native';
+
+const Card = () => {
+    return (
+        <ReactNative.View>
+            <ReactNative.Text>
+                CARD (EJEMPLO)
+            </ReactNative.Text>
+        </ReactNative.View>
+    )
+};
+
+export default  Card;// default porque va ser el unico componente generado
